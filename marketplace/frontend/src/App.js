bbbec21e20fc2343bd2923ab23cea8e0710a525c
mikeasta <thenaitrise@gmail.com>
2023-11-16 21:34:40 +0300

    #1: tutorial

--- marketplace/frontend/src/App.js
+++ marketplace/frontend/src/App.js
@@ -1,9 +1,17 @@
-const App = () => {
+import React from "react"
+
+const App = ({data}) => {
+	
   	return (
     	<div className="App">
-    		<p>Hello React!</p>
+    		<p>{data}</p>
+			<Title data="qwert"/>
     	</div>
   	);
+}
+
+const Title = ({data}) => {
+	return (<p>123, {data}</p>)
 }
 
 export default App;
