641b0bc74a1a0b70c6b04625e5f10f7ea95e8d8b
Igor Nagibin <ignagibin2014@mail.ru>
2023-12-24 09:43:28 +0300

    Добавлено представленние данных для статистики игры

---
+++ marketplace/backend/models/gamesStatistic.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+
+const messageSchema = new mongoose.Schema({
+    message_id : {
+        type: mongoose.Types.ObjectId,
+        required: true
+    },
+    created_at : {
+        type: Number,
+        required: true,
+        default: Date.now
+    },
+    sender_login : {
+        type: String,
+        required: true
+    },
+    text : {
+        type: String,
+        required: true
+    },
+})
+
+const gameStatisticSchema = new mongoose.Schema({
+    login : {
+        type: String,
+        required: true
+    },
+    support_login : {
+        type: String,
+        required: true
+    },
+
+    created_at : {
+        type: Number,
+        required: true,
+        default: Date.now
+    },
+
+    status : {
+        type: String,
+        required: true
+    },
+
+    description : {
+        type: String,
+        required: true
+    },
+
+    messages : {
+        type: [messageSchema],
+        required: false
+    },
+
+
+})
